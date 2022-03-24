e769206798a2969ab0cbe5f6dea17585c63855fc
Busna27 <lucabusnelli2001@gmail.com>
2022-03-24 15:29:07 +0100

    cicled the email creation

--- js/script.js
+++ js/script.js
@@ -10,8 +10,10 @@ const app = new Vue (
 
         methods : {
             genBtn: function() {
-                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-                .then(reply => ( this.emails.push(reply.data.response) ))
+                for (let i = 0; i < 10; i++) {
+                    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then(reply => ( this.emails.push(reply.data.response) ))
+                }
             }
         }
     }
